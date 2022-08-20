5c648a5a15dc82ddeb59c8ae2c5418d9fafa5001
abhishek986873 <110660543+abhishek986873@users.noreply.github.com>
2022-08-20 18:50:09 +0530

    Add files via upload

---
+++ grade calculator.py
@@ -0,0 +1,28 @@
+print('* INSTRUCTIONS')
+print('-----------------------------------------------------------------------------')
+print('* EACH SUBJECT IS OF 25 MARKS')
+print('-----------------------------------------------------------------------------')
+m1_maths=int(input('• ENTER YOUR MATHS MARKS: '))
+m2_physics=int(input('• ENTER YOUR PHYSICS MARKS: '))
+m3_chemistry=int(input('• ENTER YOUR CHEMISTRY MARKS: '))
+m4_ip=int(input('• ENTER YOUR IP MARKS: '))
+
+m_total=(m1_maths+m2_physics+m3_chemistry+m4_ip)
+print('-----------------------------------------------------------------------------')
+if (m_total>=90 and m_total<=100):
+    print('• YOU GOT EXCELLENT GRADE')
+if (m_total>=80 and m_total<=89):
+    print("• YOU GOT 'A' GRADE")
+if (m_total>=70 and m_total<=79):
+    print("• YOU GOT 'B' GRADE")
+if (m_total>=60 and m_total<=69):
+    print("• YOU GOT 'C' GRADE ")
+if (m_total>=50 and m_total<=59):
+    print("• YOU GOT 'D' GRADE ")
+
+
+
+        
+
+
+
