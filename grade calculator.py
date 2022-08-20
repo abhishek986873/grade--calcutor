e48eab0e54206e6675c067eebe1ec0282f1c83e8
abhishek986873 <110660543+abhishek986873@users.noreply.github.com>
2022-08-20 18:59:29 +0530

    Update grade calculator.py

--- grade calculator.py
+++ grade calculator.py
@@ -6,16 +6,17 @@ m1_maths=int(input('• ENTER YOUR MATHS MARKS: '))
 m2_physics=int(input('• ENTER YOUR PHYSICS MARKS: '))
 m3_chemistry=int(input('• ENTER YOUR CHEMISTRY MARKS: '))
 m4_ip=int(input('• ENTER YOUR IP MARKS: '))
-
 m_total=(m1_maths+m2_physics+m3_chemistry+m4_ip)
 print('-----------------------------------------------------------------------------')
 if (m_total>=90 and m_total<=100):
     print('• YOU GOT EXCELLENT GRADE')
-if (m_total>=80 and m_total<=89):
+elif (m_total>=80 and m_total<=89):
     print("• YOU GOT 'A' GRADE")
-if (m_total>=70 and m_total<=79):
+elif (m_total>=70 and m_total<=79):
     print("• YOU GOT 'B' GRADE")
-if (m_total>=60 and m_total<=69):
+elif (m_total>=60 and m_total<=69):
     print("• YOU GOT 'C' GRADE ")
-if (m_total>=50 and m_total<=59):
+elif (m_total>=50 and m_total<=59):
     print("• YOU GOT 'D' GRADE ")
+else:
+    print('• YOUR PERFOMANCE IS WORSE')
